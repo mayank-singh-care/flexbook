808a5e1b688fd1e008e8a1dc1ab3de3b9b3be2fe
max <loginfuture1@gmail.com>
2022-11-21 23:46:09 +0530

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,63 @@
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {PostService} from '../../services/post.service';
+import {Subscription} from 'rxjs';
+import {AuthService, UserData} from '../../services/auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  images: any[] = [
+    'https://images-na.ssl-images-amazon.com/images/I/51DR2KzeGBL._AC_.jpg',
+    'https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2017/07/18/18/24/dove-2516641_960_720.jpg',
+    'https://cdn.pixabay.com/photo/2017/07/18/18/24/dove-2516641_960_720.jpg',
+    'https://c0.wallpaperflare.com/preview/956/761/225/5be97da101a3f.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/9/9a/Swepac_FB_465%2C_RV70%2C_with_passing_lorry.jpg'
+  ];
+  subs: Subscription[] = [];
+  posts: any[] = [];
+  comments: any[] = [];
+  user: UserData;
+
+  constructor(private postService: PostService,
+              private authService: AuthService) {
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.subs.push(this.postService.getAllPosts().subscribe(async (posts) => {
+      this.posts = posts;
+      console.log(posts);
+    }));
+
+    this.subs.push(this.authService.CurrentUser().subscribe(user => {
+      this.user = user;
+      console.log(user);
+    }));
+
+  }
+
+  postMessage(form: NgForm): void {
+    const { message, imgUrl } = form.value;
+    console.log(message, imgUrl)
+    this.postService.postMessage(imgUrl,message,
+      `${this.user.firstName} ${this.user.lastName}`,
+      {
+        avatar: this.user.avatar,
+        lastName: this.user.lastName,
+        firstname: this.user.firstName
+      },
+    );
+    form.resetForm();
+  }
+  addComment(c) {
+    console.log(c.value.comment);
+    this.comments.push(c.value.comment);
+}
+  logout(): void {
+    this.authService.Logout();
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    projectId: 'socialbook-c337d',
+    appId: '1:257452781641:web:b7f9e618e434ec502c8f98',
+    storageBucket: 'socialbook-c337d.appspot.com',
+    apiKey: 'AIzaSyDvzXpTiY4if8MRb-9zyN-by1d6T75Hs44',
+    authDomain: 'socialbook-c337d.firebaseapp.com',
+    messagingSenderId: '257452781641',
+    measurementId: 'G-KLJNVWP5S1',
+  }
+};
